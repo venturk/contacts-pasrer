79174a598537e770b4324c99065d41433b3a87f0
venturk <33842147+venturk@users.noreply.github.com>
2018-12-31 19:30:42 +0200

    Added a few comments
    I've added some documentation to the Class to clerify as much as possible.

--- Contact.py
+++ Contact.py
@@ -32,22 +32,21 @@ class Contact(object):
         return str
 
     def show_image(self):
-        self.save_image()
+        self.save_image()  # Save the image (if not saved before)
         img_name = self.full_name + '.jpg'
 
-        if isfile(img_name):
+        if isfile(img_name):  # If the image does exist (i.e if created successfully)
             webbrowser.open(img_name)
 
     def save_image(self):
-        if self.image:
-            img_name = self.full_name + '.jpg'
+        if self.image:  # If there is any base64 image data
+            img_name = self.full_name + '.jpg'  # Init image file name
 
-            if not isfile(img_name):
-                img_data = b64decode(self.image)
+            if not isfile(img_name):  # If the image hasn't been created before
+                img_data = b64decode(self.image)  # Decode the image data from base64
 
                 with open(img_name, 'wb') as f:
-                    f.write(img_data)
-                f.close()
+                    f.write(img_data)  # Saving the image file (i.e writing the image data)
 
     @property
     def time(self):
@@ -55,6 +54,7 @@ class Contact(object):
 
     @time.setter
     def time(self, value):
+        # Converting a UNIX Timestamp to Formatted Date String
         self._call_logs = datetime.fromtimestamp(int(value)).strftime('%Y-%m-%d %H:%M:%S')
 
     @property
@@ -71,7 +71,7 @@ class Contact(object):
 
     @first_name.setter
     def first_name(self, value):
-        self._first_name = sub(r'[^\x00-\x7F]+', ' ', value)
+        self._first_name = sub(r'[^\x00-\x7F]+', ' ', value)  # Remove any non-ascii spaces
 
     @property
     def last_name(self):
@@ -79,7 +79,7 @@ class Contact(object):
 
     @last_name.setter
     def last_name(self, value):
-        self._last_name = sub(r'[^\x00-\x7F]+', ' ', value)
+        self._last_name = sub(r'[^\x00-\x7F]+', ' ', value)  # Remove any non-ascii spaces
 
     @property
     def full_name(self):
@@ -87,4 +87,4 @@ class Contact(object):
 
     @full_name.setter
     def full_name(self, value):
-        self._full_name = value.strip()
+        self._full_name = value.strip()  # Remove any redundant white spaces
